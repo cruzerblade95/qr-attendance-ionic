1a9ff7450740bb55d68484a63ecf9ab3df0d416e
NabilAjwad <nrosedi@novatis.com.my>
2021-04-22 21:47:22 +0800

    env added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB553yQyZrkHBWSX8RRLT3nEoGbmt_Oi2o",
+    authDomain: "flutter-attendance-6ef70.firebaseapp.com",
+    projectId: "flutter-attendance-6ef70",
+    storageBucket: "flutter-attendance-6ef70.appspot.com",
+    messagingSenderId: "857311098142",
+    appId: "1:857311098142:web:5cc7d87e3f6a33f51a850f"
+  }
 };
 
 /*
