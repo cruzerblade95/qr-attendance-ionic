e3439e6b83dc3d2c9d4d778a1a19b4d1501f33ca
NabilAjwad <nrosedi@novatis.com.my>
2021-04-22 21:51:05 +0800

    Login page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
